b3a468db70b8f14ab46e7a90119ad01f9e8d7c6d
dhanuvagman006 <dhanushinsit@gmail.com>
2026-04-29 11:37:11 +0530

    edited bat

--- start_all.bat
+++ start_all.bat
@@ -6,10 +6,14 @@ echo Press Ctrl+C to stop both servers.
 echo.
 
 
-echo [1/2] Activating Virtual Environment and Starting Backend Server...
+echo [1/3] Activating Virtual Environment and Training Model...
+call venv\Scripts\activate
+python backend\app\ml\train.py
+
+echo [2/3] Starting Backend Server...
 start /B "" cmd /c "call venv\Scripts\activate && cd backend && pip install -r requirements.txt && python -m uvicorn app.main:app --reload"
 
-echo [2/2] Installing Dependencies and Starting Frontend Server...
+echo [3/3] Installing Dependencies and Starting Frontend Server...
 cd frontend
 call npm install
 npm run dev
